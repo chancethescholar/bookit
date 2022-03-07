ec6540cac1af013e1856801b8f82bea2abf33e0a
chancethescholar <chanceonyiorah@gmail.com>
2022-03-06 23:31:11 -0500

    Added floating icon to scroll to top

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,20 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 const Footer = () => {
+  // This function will scroll the window to the top
+ const scrollToTop = () => {
+   window.scrollTo({
+     top: 0,
+     behavior: 'smooth' // for smoothly scrolling
+   });
+ };
+ 
   return (
     <footer
-      className="text-black text-center mainback bg-slate-100 pt-8 pb-8"
+      className="text-black text-center mainback bg-slate-100 pt-12 pb-8"
       style={{
         width: "100%",
         position: "relative",
@@ -16,6 +26,9 @@ const Footer = () => {
           Copyright © Chance Onyiorah
         </Box>
       </Container>
+      <Fab sx={{left: "auto", right: 20, bottom: 20, top: 'auto', position: "fixed"}} onClick={scrollToTop} color="primary">
+        <NavigationIcon />
+      </Fab>
     </footer>
   )
 }
--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container';
 
  const MainScreen = ({title, children}) => {
    return (
-     <div className="mainback bg-slate-100" style={{minHeight: "93vh"}}>
+     <div id="top" className="mainback bg-slate-100" style={{minHeight: "93vh"}}>
       <Container>
         <div class="grid grid-rows-1">
           <div className="page text-black pt-8 pl-8 font-mono">
